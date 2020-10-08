db0810ab23b665b044b33ea5646aea06fc687db7
keshavanandsingh <keshavanand22@gmail.com>
2020-10-09 01:00:00 +0530

    added

--- src/com/day13/MyLinkedList.java
+++ src/com/day13/MyLinkedList.java
@@ -1,5 +1,47 @@
 package com.day13;
 
 public class MyLinkedList<K> {
+	INode<K> head;
+	INode<K> tail;
 
+	public MyLinkedList() {
+		this.head = null;
+		this.tail = null;
+	}
+
+	public void add(MyNode<Integer> myThreeNode) {
+		if (this.tail == null)
+			this.tail = myThreeNode;
+		if (this.head == null) {
+			this.head = myThreeNode;
+		} else {
+			INode<K> tempNode = this.head;
+			this.head = myThreeNode;
+			this.head.setNext(tempNode);
+		}
+	}
+
+	public void append(MyNode<Integer> myFirstNode) {
+		if (this.head == null)
+			this.head = myFirstNode;
+		if (this.tail == null) {
+			this.tail = myFirstNode;
+		} else {
+			this.tail.setNext(myFirstNode);
+			this.tail = myFirstNode;
+		}
+	}
+
+	public void print() {
+		StringBuffer myNodes = new StringBuffer();
+		INode<K> tempNode = head;
+		while (tempNode.getNext() != null) {
+			myNodes.append(tempNode.getKey());
+			if (!tempNode.equals(tail))
+				myNodes.append("->");
+			tempNode = tempNode.getNext();
+		}
+		myNodes.append(tempNode.getKey());
+		System.out.println(myNodes);
+	}
 }
--- src/com/day13/MyLinkedListTest.java
+++ src/com/day13/MyLinkedListTest.java
@@ -1,14 +1,38 @@
 package com.day13;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 class MyLinkedListTest {
+	@Test
+	public void given3NumbersWhenAddedToLinkedListShouldBeAddedToTop() {
+		MyNode<Integer> myFirstNode = new MyNode<>(70);
+		MyNode<Integer> mySecondNode = new MyNode<>(30);
+		MyNode<Integer> myThreeNode = new MyNode<>(56);
+		MyLinkedList<Integer> myLinkedList = new MyLinkedList<>();
+		myLinkedList.add(myFirstNode);
+		myLinkedList.add(mySecondNode);
+		myLinkedList.add(myThreeNode);
+		myLinkedList.print();
+		boolean result = myLinkedList.head.equals(myThreeNode) && myLinkedList.head.getNext().equals(mySecondNode)
+				&& myLinkedList.tail.equals(myFirstNode);
+		assertTrue(result);
+	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void given3NumbersWhenAppendedShouldBeAddedToLast() {
+		MyNode<Integer> myFirstNode = new MyNode<>(56);
+		MyNode<Integer> mySecondNode = new MyNode<>(30);
+		MyNode<Integer> myThreeNode = new MyNode<>(70);
+		MyLinkedList<Integer> myLinkedList = new MyLinkedList<>();
+		myLinkedList.append(myFirstNode);
+		myLinkedList.append(mySecondNode);
+		myLinkedList.append(myThreeNode);
+		myLinkedList.print();
+		boolean result = myLinkedList.head.equals(myFirstNode) && myLinkedList.head.getNext().equals(mySecondNode)
+				&& myLinkedList.tail.equals(myThreeNode);
+		assertTrue(result);
 	}
 
 }
