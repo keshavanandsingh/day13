5685d0c283088e108edeb63fa635220932d16627
keshavanandsingh <keshavanand22@gmail.com>
2020-10-09 01:57:49 +0530

    added

--- src/com/StackAndQueueImplementation/MyQueueTest.java
+++ src/com/StackAndQueueImplementation/MyQueueTest.java
@@ -20,4 +20,20 @@ class MyQueueTest {
 		myQueue.printQueue();
 		Assert.assertEquals(firstNode, top);
 	}
+
+	@Test
+	public void given3NumberInQueueWhenDequeuedShouldMatchedFirstAddedNode() {
+		MyNode<Integer> firstNode = new MyNode(56);
+		MyNode<Integer> secondNode = new MyNode(30);
+		MyNode<Integer> thirdNode = new MyNode(70);
+
+		MyQueue myQueue = new MyQueue();
+		myQueue.enqueue(firstNode);
+		myQueue.enqueue(secondNode);
+		myQueue.enqueue(thirdNode);
+
+		AllNode dequeuedNode = myQueue.dequeue();
+		myQueue.printQueue();
+		Assert.assertEquals(firstNode, dequeuedNode);
+	}
 }
